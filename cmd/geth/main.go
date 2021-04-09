bbf92b11c0fef64285aac5db17e6101ac3dc297a
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-04-09 10:57:46 -0400

    [flashbots] Force both workers to produce task - timer protects plainworker
    [flashbots] Stricter uniqueness for task - guard against nil pointer when timer go off
    
    [flashbots] Map becomes set - fresh set at len 4096
    
    [flashbots] profit switch cli flag descr
    
    [flashbots] Straight to work if ProfitWait == 0

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -128,6 +128,7 @@ var (
 		utils.MinerExtraDataFlag,
 		utils.MinerRecommitIntervalFlag,
 		utils.MinerNoVerfiyFlag,
+		utils.MinerStrictProfitSwitchFlag,
 		utils.NATFlag,
 		utils.NoDiscoverFlag,
 		utils.DiscoveryV5Flag,
